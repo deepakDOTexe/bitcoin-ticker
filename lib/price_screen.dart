5772b271dd1b76955561be56542f4c01a3756206
deepakDOTexe <iit2018116@iiita.ac.in>
2020-06-05 22:49:16 +0530

    add drop down menu items

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +7,21 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+
+  String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem<String>> getDropDownList(){
+    List<DropdownMenuItem<String>> dropDownList;
+    for(String currency in currenciesList){
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropDownList.add(newItem);
+    }
+    return dropDownList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +58,15 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: getDropDownList(),
+              onChanged: (value){
+                setState(() {
+                  selectedCurrency = value;
+                });
+              },
+            ),
           ),
         ],
       ),
